3655e11c076cd0582e96e04f93c9eefce7f057bb
brotchu <nithint216@gmail.com>
2021-01-19 14:49:57 +0530

    init

---
+++ cmd/root.go
@@ -0,0 +1,8 @@
+package cmd
+
+import "github.com/spf12/cobra"
+
+var RootCmd := &cobra.Command{
+	Use: "tm",
+	Short: "tm is cli Task Master",
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/Brotchu/tm/cmd"
+
+func main() {
+	cmd.RootCmd.Execute()
+}
